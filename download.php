59a39a3f7ec404df8c7595cdae7caf25ebba0216
alve89 <info@stefan-herzog.com>
2016-11-11 09:18:02 +0100

    Remove infile
    Removed $infile because it is already deleted in index.php

--- download.php
+++ download.php
@@ -1,22 +1,20 @@
 <?php
-	$infilename = __DIR__ . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'profile.mobileconfig';
 	$outfilename = __DIR__ . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'profile-sig.mobileconfig';
 	if(isset($_GET['error']))
 	{
 		//var_dump($_SERVER["HTTP_REFERER"]);
-		die('Das Profile enth&auml;lt nicht alle notwendigen Daten. Bitte kontaktiere den Administrator.');
+		die('Profile does not contain all relevant data, please contact administrator');
 	}
 	else
 	{
-		// Einlesen der signierten Datei und Ausgabe als Download
+		// Read created profile file
 		$file = file_get_contents($outfilename);
 	
 		header('Content-type: application/octet-stream; charset=utf-8');
 		header('Content-Disposition: attachment; filename="profile-sig.mobileconfig"');
 		echo $file;
-		// Löschen der erstellten Dateien
-		unlink($infilename);
-		//unlink($outfilename);	
+		// Remove created files
+		// unlink($outfilename); // Commented to be able to use direct link
 	}
 
 	exit;
