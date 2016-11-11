b0b0e574e5ca4e4130b42f876e64ced2c9ee9a5d
alve89 <info@stefan-herzog.com>
2016-11-11 08:14:22 +0100

    Update config.getUser.php

--- config/config.getUser.php
+++ config/config.getUser.php
@@ -6,8 +6,10 @@
 	// "SELECT * FROM `users` WHERE 'username' = $usernameFromLoginForm";
 	
 	// Apply result from MySQL to object $user
+	// If you filled mySQLResultAsObject from database, it's automatically an object (when fetching an object).
+	$mySQLResultAsObject = new stdClass;  // This line is only for testing purposes to not overwrite already in index.php defined $user and have not an object anymore.
 	// Variable name needs to be $user!!!
-	$user = $mySQLResultAsObject;
+	$user = $mySQLResultAsObject;;
 	
 	/*
 	 *	This might contain:
