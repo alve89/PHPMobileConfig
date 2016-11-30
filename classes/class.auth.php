cb70619be481c92564a58872d9972fc70e06069e
alve89 <info@stefan-herzog.com>
2016-11-30 09:43:51 +0100

    Update class.auth.php

--- classes/class.auth.php
+++ classes/class.auth.php
@@ -1,18 +1,38 @@
 <?php
+	/*
+	 * Authentication of the user
+	 * 
+	 * @param 	$uid	string	Username, given by form
+	 * @param	$pw		string	Password, given by form
+	 * @param	$return	boolean	Contains the result of authentication
+	 * 
+	 *
+	 */
 
-	if(!class_exists('auth'))
+	// Parent class that is called
+	class auth
 	{
-		class auth
+		public function authUser($uid, $pw)
 		{
-			public static function authUser($uid, $pw)
+			$classes = get_declared_classes();
+
+			foreach($classes as $class)
 			{
-				# 
-				# Add any authentication algorithms here
-				#
-				$auth = true;
-				
-				// $auth needs to be boolean
-				return $auth;
+				$reflectionClass = new ReflectionClass($class);
+				if($reflectionClass->isSubClassOf(__CLASS__))
+				{
+					$children[] = $class;
+				}
 			}
+
+			foreach($children as $class)
+			{
+				$auth = new $class;
+				if($return = $auth->authBackend($uid, $pw))
+				{
+					break;
+				}
+			}
+			return $return;
 		}
 	}
